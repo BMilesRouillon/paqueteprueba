c303c2fa02659306fab6dbe93edefa61d27bddd6
BenjamilesAustral <bmiles@mail.austral.edu.ar>
2024-10-04 09:32:32 -0300

    comenta funcion suma

--- R/suma.R
+++ R/suma.R
@@ -1,15 +1,24 @@
-#The sum of two numbers
-library(tidyverse)
-#sum <- function(x=2,y=2) {
-  #return(x+y)
-#}
-
-
-#sum <- function(x, y) {
- #ifelse(is.numeric(x) & is.numeric(y), return(x+y), "ERROR!, deben ser numéricos")
-#}
-
-sum <- function(x, y) {
+#'The sum of two numbers
+#'
+#'This function `suma()` takes two numbers and returns their sum. It does not accept negative values and provides a warning if a non-numeric input is provided.
+#'
+#'This are extra details of this function
+#'
+#'@param x It is a positive numeric value.
+#'@param y It is a positive numeric value.
+#'
+#' @return
+#' A numeric vector containing the sum of `x` and `y`.
+#'
+#' @examples
+#' suma(1,1)
+#' suma(-1,1)
+#' suma("1",1)
+#' suma(30,50)
+#'
+#'
+#' @export
+suma <- function(x, y) {
   if (!is.numeric(x) | !is.numeric(y)) {
     return("ERROR!, deben ser numéricos")
   }
